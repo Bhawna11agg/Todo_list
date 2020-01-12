5ff997533eb8c2338f4ac820c9f5920175ea64bf
Bhawna11agg <bhawna11agg@gmail.com>
2020-01-13 00:48:57 +0700

    update

---
+++ app/src/main/java/com/example/todo_list/Adapter3.java
@@ -0,0 +1,54 @@
+package com.example.todo_list;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class Adapter3 extends RecyclerView.Adapter<Adapter3.ViewAdapter> {
+       ArrayList<String> arrayList;
+    public Adapter3(ArrayList<String> arrayList1) {
+            this.arrayList = arrayList1;
+        }
+
+        @NonNull
+        @Override
+        public ViewAdapter onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            LayoutInflater inflater=LayoutInflater.from(parent.getContext());
+            View itemView=inflater.inflate(R.layout.list_items,parent,false);
+            return new ViewAdapter(itemView);
+        }
+
+    @Override
+    public void onBindViewHolder(@NonNull final ViewAdapter holder, final int position) {
+        holder.txt1.setText(arrayList.get(position));
+        holder.btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+    }
+        @Override
+        public int getItemCount() {
+            return arrayList.size();
+        }
+
+        public class ViewAdapter extends RecyclerView.ViewHolder{
+            TextView txt1;
+            Button btn;
+            public ViewAdapter(@NonNull View itemView) {
+                super(itemView);
+                txt1=(TextView)itemView.findViewById(R.id.textView);
+                btn=(Button)itemView.findViewById(R.id.button);
+            }
+        }
+    }
+
---
+++ app/src/main/java/com/example/todo_list/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.todo_list;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity{
+    ArrayList<String> arrayList = new ArrayList<>();
+    EditText edt;
+    Button btn;
+    RecyclerView my_recycle;
+    RecyclerView.Adapter adapter;
+    int count = 0;
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn = findViewById(R.id.buttonAdd);
+        edt = findViewById(R.id.text);
+        my_recycle = findViewById(R.id.my_recycle);
+        my_recycle.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+        adapter = new Adapter3(arrayList);
+        my_recycle.setAdapter(adapter);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                arrayList.add(edt.getText().toString());
+                adapter.notifyDataSetChanged();
+                edt.setText("");
+            }
+        });
+    }
+}
